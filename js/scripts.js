092e1d697cf69f42d3ba03e3f47524959ec5b446
kelvinngugi <njugunakevngugi@gmail.com>
2018-02-13 11:27:00 +0300

    add divide

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var multiply = function(number1, number2) {
-return number1 * number2;
+var divide = function(number1, number2) {
+return number1 / number2;
 };
     
 var number1 = parseInt(prompt("Enter a number"));
 var number2 = parseInt(prompt("Enter another number"));
     
-alert(multiply(number1, number2));
+alert(divide(number1, number2));
