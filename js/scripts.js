153ec82202d684eb82f1ab20a8251082adcc15c0
kelvinngugi <njugunakevngugi@gmail.com>
2018-02-13 11:23:22 +0300

    add multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var subtract = function(number1, number2) {
-return number1 - number2;
+var multiply = function(number1, number2) {
+return number1 * number2;
 };
     
 var number1 = parseInt(prompt("Enter a number"));
 var number2 = parseInt(prompt("Enter another number"));
     
-alert(subtract(number1, number2));
+alert(multiply(number1, number2));
